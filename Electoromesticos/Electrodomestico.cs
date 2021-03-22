3aa86ffa8f5c8a14a3f697161a53ee1c3ad4ebb3
davidbarberan9 <davidbarberan9@gmail.com>
2021-03-22 20:52:35 +0100

    Agregar archivos de proyecto.

---
+++ Electoromesticos/Electrodomestico.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electrodomesticos
+{
+    #region Electrodomestico
+    public class Electrodomestico
+    {
+        #region atributos
+        //informacion valida
+        public enum colores { blanco, negro, rojo, azul, gris };
+        public enum consumoElectrico { A, B, C, D, E, F };
+        //Datos por defecto
+        const colores colorD = colores.blanco;
+        const char consumoD = 'F';
+        const decimal precioBaseD = 100;
+        const int pesoD = 5;
+
+        private decimal _precioBase;
+        private colores _color;
+        private consumoElectrico _consumo;
+        private int _peso;
+        #endregion
+        #region Getters
+        public decimal PrecioBase { get => _precioBase; set => _precioBase = value; }
+        public colores Color { get => _color; set => _color = value; }
+        public consumoElectrico Consumo { get => _consumo; set => _consumo = value; }
+        public int Peso { get => _peso; set => _peso = value; }
+        #endregion
+        #region Constructors
+        public Electrodomestico() : this(precioBaseD, pesoD)    
+        { }        
+        public Electrodomestico(decimal precio,int peso) : this(precio,consumoD , colorD.ToString(), peso)
+        {}
+        public Electrodomestico(decimal precio,char consumo,String color, int peso)
+        {
+            _precioBase = precio;
+            _consumo = comprobarConsumoEnergetico(consumo);
+            _color = comprobarColor(color);
+            _peso = peso;
+
+        }
+        #endregion
+        #region metodos
+        public override string ToString()
+        {
+            return "precio="+_precioBase+" consumo="+_consumo+" color="+_color+" Peso en KG="+_peso;
+        }
+
+        public consumoElectrico comprobarConsumoEnergetico(char letra)
+        {
+            consumoElectrico consumo;
+            if(Enum.TryParse(letra+"", out consumo))
+            {
+                consumo=Enum.Parse<consumoElectrico>(letra+"");
+            }
+            else
+            {
+                consumo = Enum.Parse<consumoElectrico>(consumoD+"");
+            }
+            return consumo;
+        }
+        public colores comprobarColor(String color)
+        {
+            colores c = colorD;
+
+            if (Enum.TryParse(color, out c))
+            {
+                c = Enum.Parse<colores>(color);
+            }
+
+            return c;
+        }
+
+        virtual public decimal precioFinal()
+        {
+            decimal precioTotal = _precioBase;
+            switch (_consumo)
+            {
+                case consumoElectrico.A:precioTotal += 100; break;
+                case consumoElectrico.B:precioTotal += 80; break;
+                case consumoElectrico.C:precioTotal += 60; break;
+                case consumoElectrico.D:precioTotal += 50; break;
+                case consumoElectrico.E:precioTotal += 30; break;
+                case consumoElectrico.F:precioTotal += 10; break;
+            }
+            switch (_peso)//case int n when (n >= 0 && n <= 25):
+                {
+                case int n when(n<=19):precioTotal += 10; break;
+                case int n when (n <= 49): precioTotal += 50; break;
+                case int n when (n <= 79): precioTotal += 80; break;
+                case int n when (n > 79): precioTotal += 100; break;
+            }
+            return precioTotal;
+        }
+        #endregion
+    }
+    #endregion
+
+    #region Lavadora
+    public class Lavadora : Electrodomestico
+    {
+        private int cargaD=5;
+        private int _carga;
+
+        public Lavadora():base()
+        {
+            Carga = cargaD;
+        }
+
+        public Lavadora(int precio, int peso) : base(precio, peso)
+        {
+            Carga = cargaD;
+        }
+        public Lavadora(int carga,decimal precio,String color,char consumo,int peso):base(precio, consumo,color, peso)
+        {
+            Carga = carga;
+        }
+
+        public int Carga { get => _carga; set => _carga = value; }
+
+        public override decimal precioFinal()
+        {
+            if (Carga >= 30)
+            {
+                return PrecioBase + 50;
+            }
+            return base.precioFinal();
+        }
+    }
+    #endregion
+
+    #region Television
+    public class Television : Electrodomestico
+    {
+        Boolean TDTD = false;
+        int resolucionD = 20;
+        int _resolucion;
+        Boolean _TDT;
+        public int Resolucion { get => _resolucion; set => _resolucion = value; }
+        public bool TDT { get => _TDT; set => _TDT = value; }
+        public Television():base()
+        {
+            _TDT = TDTD;
+            _resolucion = resolucionD;
+        }
+        public Television(decimal precio,int peso):base(precio,peso)
+        {
+            _TDT = TDTD;
+            _resolucion = resolucionD;
+        }
+        public Television(int res,Boolean tdt, decimal precio, char consumo, String color, int peso):base(precio,consumo,color,peso)
+        {
+            _TDT = tdt;
+            _resolucion = res;
+        }
+
+        public override decimal precioFinal()
+        {
+            decimal precioFinal= base.precioFinal();
+            if (_resolucion > 40)
+                precioFinal = precioFinal* decimal.Parse("1.3");
+
+            if (_TDT)
+                precioFinal += 50;
+
+            return precioFinal;
+        }
+    }
+    #endregion
+}
---
+++ Electoromesticos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Electrodomesticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Electrodomestico[] electrodomesticos = new Electrodomestico[10];
+            electrodomesticos[0] = new Electrodomestico();
+            electrodomesticos[1] = new Electrodomestico(200,40);
+            electrodomesticos[2] = new Electrodomestico(150,'C',"Blanco",30);
+            electrodomesticos[3] = new Lavadora();
+            electrodomesticos[4] = new Lavadora(300,80);
+            electrodomesticos[5] = new Lavadora(10,250, "NEGRO",'F',70);
+            electrodomesticos[6] = new Television();
+            electrodomesticos[7] = new Television(400,10);
+            electrodomesticos[8] = new Television(45,true,500,'F',"RoJo",8);
+            electrodomesticos[9] = new Electrodomestico();
+            decimal precioTotalElectrodomesticos=0,
+                precioTotalLavadoras=0,
+                precioTotalTelevision=0;
+            for (int i = 0; i < electrodomesticos.Length; i++)
+            {
+               // Console.WriteLine(electrodomesticos[i].precioFinal() + " "+electrodomesticos[i].GetType());
+                decimal precio = electrodomesticos[i].precioFinal();
+                if (electrodomesticos[i].GetType() == typeof(Lavadora))
+                    precioTotalLavadoras += precio;
+                else if (electrodomesticos[i].GetType() == typeof(Television))
+                    precioTotalTelevision += precio;
+
+                precioTotalElectrodomesticos += precio;
+            }
+            Console.WriteLine(precioTotalElectrodomesticos + " ELECTRODOMESTICOS");
+            Console.WriteLine(precioTotalLavadoras + " LAVADORAS");
+            Console.WriteLine(precioTotalTelevision + " TELEVISIONES");
+        }
+    }
+}
